e03e19b6359899662bb2363f5fbcd574dbc1ec85
sergio <sergio.vieira@fatec.sp.gov.br>
2025-06-11 19:09:40 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MauiAppFit.Helpers;
+
+namespace MauiAppFit
+{
+
+    public partial class App : Application
+    {
+        static SQLiteDataBaseHelper database;
+
+            public static SQLiteDataBaseHelper Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new SQLiteDataBaseHelper(
+                        Path.Combine(Environment.GetFolderPath(                          
+                        Environment.SpecialFolder.LocalApplicationData),
+                        "XampAppFit.db3"));
+                }
+
+                return database;
+            }
+        }
+    
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
